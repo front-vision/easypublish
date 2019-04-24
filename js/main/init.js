70d7c16b2e28f1dc31177120fdb6d8f15389ac91
vxgganxv <vxgganxv@naver.com>
2019-04-24 19:41:37 +0900

    layout

---
+++ js/main/init.js
@@ -0,0 +1,11 @@
+$(function() {
+    $(".center").slick({
+        dots: false,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 5,
+        slidesToScroll: 3
+    });
+    $(".visual-slider").slick({
+    });
+});
