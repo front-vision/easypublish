3ede0110530d93a30d971a77ad6652e4ca0e729a
vxggangxv <vxggangxv@naver.com>
2019-05-08 15:29:49 +0900

    issue slick

--- src/js/00_Main/index.js
+++ src/js/00_Main/index.js
@@ -1,11 +1,29 @@
-// TOP BANNER
 $(function() {
+    // TOP BANNER
     $('.top_banner_close').click(function(){
         $('.top_banner').slideUp();
     });
+    
+    // MAIN VISUAL
+    $(".visual_slider").slick({
+        arrows: false,
+        autoplay: true,
+        fade: true,
+        infinite: true,
+        speed: 1000
+    });
+
+    // BOOK LIST
+    $(".book_slider").slick({
+        arrows: false,
+        autoplay: true,
+        speed: 2000,
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 3
+
+    });
 });
 
-// MAIN VISUAL
 
 
-// BOOK LIST
