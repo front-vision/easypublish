a3ccae2c70eaed9992703dc4237dc30b7ded9fd9
byesus14 <byesus14@hotmail.com>
2019-05-09 12:31:34 +0900

    wow

--- src/js/00_Main/index.js
+++ src/js/00_Main/index.js
@@ -6,6 +6,26 @@ $(function() {
 });
 
 // MAIN VISUAL
-
+$(function() {
+    $(".visual_slider").slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        fade: true,
+        infinite: true,
+        speed: 1000
+    })
+});
 
 // BOOK LIST
+$(function() {
+    $(".book_slider").slick({
+        arrows: false,
+        autoplay: true,
+        speed: 700,
+        centerMode: true,
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 3
+    })
+});
